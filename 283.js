87767dcfbd44ac2894b20089eb6a0a28451ae1f1
Fayis KV <mohammedfayis135@gmail.com>
2025-07-19 17:56:59 +0530

    leetcode

---
+++ 283.js
@@ -0,0 +1,17 @@
+var moveZeroes = function(nums) {
+    var a = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[a] = nums[i];
+            a++;
+        }
+    }
+    while (a < nums.length) {
+        nums[a] = 0;
+        a++;
+    }
+};
+
+var nums = [0, 1, 0, 3, 12];
+moveZeroes(nums);
+console.log(nums); //[1, 3, 12, 0, 0]
