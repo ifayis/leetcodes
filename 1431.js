216ba88b632c69c3af23686ffddc96a64aa03510
Fayis KV <mohammedfayis135@gmail.com>
2025-07-23 11:51:32 +0530

    leetcode

---
+++ 1431.js
@@ -0,0 +1,14 @@
+var kidsWithCandies = function(candies, extraCandies) {
+    let big = Math.max(...candies)
+    let arr = []
+    for (let i=0; i<candies.length; i++){
+        if(candies[i] + extraCandies >= big){
+             arr.push(true) 
+        }else{
+             arr.push(false)
+        }
+    }
+    return arr
+};
+
+console.log(kidsWithCandies([2,3,5,1,3]))
