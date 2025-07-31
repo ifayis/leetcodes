5bae9b329bcf467c1fc56c6279651b62af7082ae
Fayis KV <mohammedfayis135@gmail.com>
2025-07-31 10:37:27 +0530

    leetcode

---
+++ 2129.js
@@ -0,0 +1,12 @@
+var capitalizeTitle = function(title) {
+    let a = title.toLowerCase().split(' ')
+    for (let i=0; i < a.length; i++){
+        if( a[i].length > 2 ){
+          a[i] = a[i][0].toUpperCase() + a[i].slice(1)
+        }
+        
+    }
+    return  a.join(' ')
+};
+
+console.log(capitalizeTitle("capiTalIze tHe titLe"));
