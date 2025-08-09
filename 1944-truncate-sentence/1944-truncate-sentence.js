9d575cf4ccef688a97b3dcd1e99724e91967b227
mhd.fayis <mohammedfayis135@gmail.com>
2025-08-09 18:36:58 +0530

    Delete 1944-truncate-sentence directory

--- 1944-truncate-sentence/1944-truncate-sentence.js
+++
@@ -1,9 +0,0 @@
-/**
- * @param {string} s
- * @param {number} k
- * @return {string}
- */
-var truncateSentence = function(s, k) {
-    let arr = s.split(' ')
-    return arr.slice(0,k).join(' ')
-};
