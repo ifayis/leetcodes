0daf30206bb6ad9fe9e93e0dfc5cafa83d4b80da
Fayis KV <mohammedfayis135@gmail.com>
2025-08-21 14:14:24 +0530

    leetcode

---
+++ 349.js
@@ -0,0 +1,11 @@
+var intersection = function(nums1, nums2) {
+    let arr =[];
+     for (let i = 0; i < nums1.length; i++) {
+        if (nums2.includes(nums1[i]) && !arr.includes(nums1[i])) {
+            arr.push(nums1[i]);
+        }
+    }
+   return arr
+};
+
+document.writeln(intersection([1,2,2,1],[2,2]))
