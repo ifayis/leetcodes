0f9ca9f5728cfd19b83efee796d86a3d3fa6e7e6
Fayis KV <mohammedfayis135@gmail.com>
2025-08-26 12:55:41 +0530

    leetcode

---
+++ 3099.js
@@ -0,0 +1,13 @@
+var sumOfTheDigitsOfHarshadNumber = function(x) {
+    let sum = 0
+    let temp = x
+    while (temp > 0) {
+        sum += temp % 10
+        temp = Math.floor(temp / 10)
+    }
+    if (x % sum === 0)
+        return sum
+    return -1
+};
+
+document.writeln(sumOfTheDigitsOfHarshadNumber(18))
